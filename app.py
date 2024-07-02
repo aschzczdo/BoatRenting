44eceb9b2c00b5807c7d00031d174097c7314d94
Degreco <antoniodaniel@estudiante.edib.es>
2024-07-02 17:32:36 +0200

    Committing changes to app.py before pulling

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
 from flask import Flask, render_template
-from routes.auth import auth_blueprint
 from dotenv import load_dotenv
-from models.db import mysql_connection
+from models.db import postgres_connection  # Update import to use postgres_connection
+from routes.auth import auth_blueprint
 from routes.e404 import error_blueprint
 from routes.gallery import gallery_blueprint
 from routes.destinations import destinations_blueprint
 from routes.main import main_blueprint
+from routes.dashboard import dashboard_blueprint
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+# Registering blueprints
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(error_blueprint)
 app.register_blueprint(gallery_blueprint)
 app.register_blueprint(destinations_blueprint)
+app.register_blueprint(main_blueprint)
+app.register_blueprint(dashboard_blueprint)
 
-
+# Home route
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-app.register_blueprint(main_blueprint)
-
-
+# Charters route
 @app.route("/charters")
 def charters():
     return render_template("charters.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
